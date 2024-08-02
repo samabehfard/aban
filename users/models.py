214d39f51ad66962d3bd2aaca4e6d6f12fb067a7
sama <raphichigo@gmail.com>
2024-08-02 15:19:17 +0330

    ADD: add User model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    username = models.CharField(max_length=100, unique=True)
 
+    def __str__(self):
+        return self.username
 # Create your models here.
