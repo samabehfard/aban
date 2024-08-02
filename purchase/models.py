9b0f21f05ce6a33d7deabcfa5e8707c4be657374
sama <raphichigo@gmail.com>
2024-08-02 15:20:12 +0330

    ADD: add Transaction and Wallet model

--- purchase/models.py
+++ purchase/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+from django.db import models
+
+from users.models import User
+
+
+class Wallet(models.Model):
+    currency_name = models.CharField(max_length=50)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='wallets')
+    amount = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.currency_name}: {self.amount}"
+
+class Transaction(models.Model):
+    amount = models.IntegerField()
+    source = models.ForeignKey(Wallet, on_delete=models.CASCADE, related_name='outgoing_transactions')
+    destination = models.ForeignKey(Wallet, on_delete=models.CASCADE, related_name='incoming_transactions')
+
+    def __str__(self):
+        return f"Transaction: {self.amount} from {self.source} to {self.destination}"
